c472ad448a2f8b25a0d384fba6e36d257758a286
suyeah <sy.han2@okestro.com>
2024-03-15 15:19:40 +0900

    state lifting 상태를 가지고 있는 부모 <-> 자식

---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIdx, colIdx) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [ ...prevGameBoard.map(innerArr => [...innerArr]) ]; // deep copy
+            updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
+            return updatedBoard;
+        });
+
+        onSelectSquare();
+    }
+
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIdx) => <li key={rowIdx}>
+                <ol>
+                    {row.map((playerSymbol, colIdx) => <li key={colIdx}><button onClick={() => handleSelectSquare(rowIdx, colIdx)}>{playerSymbol}</button></li>)}
+                </ol>
+            </li>)}
+        </ol>
+    );
+}
