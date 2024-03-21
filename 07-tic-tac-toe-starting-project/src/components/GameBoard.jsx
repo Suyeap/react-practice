b41c61136d8273fbdc944bdec9e3f41546efd3a8
suyeah <sy.han2@okestro.com>
2024-03-21 14:33:37 +0900

    props state 파생

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,29 +1,24 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIdx, colIdx) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [ ...prevGameBoard.map(innerArr => [...innerArr]) ]; // deep copy
-            updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
-            return updatedBoard;
-        });
-
-        onSelectSquare();
-    }
-
+export default function GameBoard({ onSelectSquare }) {
+    // const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+    // function handleSelectSquare(rowIdx, colIdx) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [ ...prevGameBoard.map(innerArr => [...innerArr]) ]; // deep copy
+    //         updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIdx) => <li key={rowIdx}>
                 <ol>
-                    {row.map((playerSymbol, colIdx) => <li key={colIdx}><button onClick={() => handleSelectSquare(rowIdx, colIdx)}>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIdx) => <li key={colIdx}><button onClick={() => onSelectSquare(rowIdx, colIdx)}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
