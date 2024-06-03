896d8dd7c724e1702c2dce8ba11c9218b0279cc7
suyeah <sy.han2@okestro.com>
2024-06-03 16:46:38 +0900

    승리무승부

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,30 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-    // const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
-    // function handleSelectSquare(rowIdx, colIdx) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [ ...prevGameBoard.map(innerArr => [...innerArr]) ]; // deep copy
-    //         updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
-    //     onSelectSquare();
-    // }
-
+export default function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIdx) => <li key={rowIdx}>
+            {board.map((row, rowIdx) => <li key={rowIdx}>
                 <ol>
                     {row.map((playerSymbol, colIdx) => (
                         <li key={colIdx}>
